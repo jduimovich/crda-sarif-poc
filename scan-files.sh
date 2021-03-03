3abe9b39c4389af1b137da271a92a2b21253fc11
John Duimovich <jduimovich@gmail.com>
2021-03-03 08:02:20 -0500

    manual run update

--- scan-files.sh
+++ scan-files.sh
@@ -1,14 +1,17 @@
 
 CRDA=/crda-sarif/crda 
+
+echo $0 called with  $1, token $2
+
+FILE=$(pwd)/$1
+SNYK_TOKEN=$(pwd)/$SNYK_TOKEN
  
 echo running  $CRDA  auth -t $SNYK_TOKEN 
-$CRDA  auth -t $SNYK_TOKEN
-FILE=$(pwd)/package.json 
-echo file asked for is $1, using $FILE
-
+$CRDA  auth -t $SNYK_TOKEN 
 echo running  $CRDA analyse -j $FILE
 $CRDA analyse -j --verbose  $FILE  > crda.json
 
-echo json generated 
+echo crda.json generated 
 cat crda.json  
+echo converting crda file to sarif
 node convert.js crda.json
