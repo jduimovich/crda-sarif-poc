6ffafd6848efd79a1d6d97a16460e94a4f182e82
John Duimovich <jduimovich@gmail.com>
2021-03-23 19:23:26 -0400

    local test script

--- hack/scan.cmd
+++ hack/scan.cmd
@@ -1,6 +1,10 @@
+@echo off
 docker build  -t jduimovich/crda-sarif . 
 
 rem docker run -it --env SNYK_TOKEN=%SNYK_TOKEN%   --entrypoint /bin/sh jduimovich/crda-sarif
  
-docker run -it -v %CD%:/scan --env SNYK_TOKEN=%SNYK_TOKEN%   jduimovich/crda-sarif package.json %SNYK_TOKEN%   
-more output.sarif | jq 
+docker run -it -v %CD%:/scan --env SNYK_TOKEN=%SNYK_TOKEN%   jduimovich/crda-sarif package.json %SNYK_TOKEN%  temp.sarif
+
+more temp.sarif | jq 
+more temp.sarif | jq  > crda.sarif
+del temp.sarif
